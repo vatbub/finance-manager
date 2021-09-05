3101d5b95484f312ba1941f0976b2cae00b73ece
Frederik Kammel <vatbub123+github@googlemail.com>
2021-09-05 20:21:00 +0200

    Implemented logging

--- src/main/kotlin/com/github/vatbub/finance/manager/logging/Logging.kt
+++ src/main/kotlin/com/github/vatbub/finance/manager/logging/Logging.kt
@@ -1,15 +1,15 @@
 /*-
  * #%L
- * finance-manager
+ * Smart Charge
  * %%
- * Copyright (C) 2019 - 2021 Frederik Kammel
+ * Copyright (C) 2016 - 2020 Frederik Kammel
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,17 +17,22 @@
  * limitations under the License.
  * #L%
  */
-module finance.manager {
-    requires javafx.graphics;
-    requires javafx.fxml;
-    requires kotlin.stdlib;
-    requires javafx.controls;
-    requires org.apache.commons.lang3;
-    requires java.logging;
-    requires exposed.core;
-    requires java.sql;
-    requires kotlin.reflect;
-    requires org.slf4j;
+package com.github.vatbub.finance.manager.logging
 
-    opens com.github.vatbub.finance.manager to javafx.graphics, javafx.fxml, javafx.base;
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+val logger by lazy {
+    LoggingHandlers.initializeIfUninitialized()
+    LoggerFactory.getLogger("Global")!!
+}
+
+val Any.logger: Logger
+    get() {
+        LoggingHandlers.initializeIfUninitialized()
+        return LoggerFactory.getLogger(this.javaClass)!!
+    }
+
+val exceptionHandler = { thread: Thread, throwable: Throwable ->
+    logger.error("Exception in thread ${thread.name}.", throwable)
 }
